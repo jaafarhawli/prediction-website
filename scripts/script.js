fab95a2a72214146ecf97cafbd60d08a71e33c8e
Jaafar Hawli <jaafarhawle@gmail.com>
2022-09-10 14:45:46 +0300

    added the age api

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
 let submit = document.getElementById('btn');
 let gender = document.getElementById('gender');
+let age = document.getElementById('age');
 
 btn.addEventListener('click', () => {
 	var name = document.getElementById('myText').value;
 	let gender_URL = `https://api.genderize.io/?name=${name}`;
+	let age_URL = `https://api.agify.io/?name=${name}`;
 
 	//Fetch the gender
 	fetch(gender_URL).then((Response) => Response.json()).then((ResponseJson) => {
 		gender.innerHTML = ResponseJson.gender;
+	});
+
+	//Fetch the age
+	fetch(age_URL).then((Response) => Response.json()).then((ResponseJson) => {
+		age.innerHTML = ResponseJson.age;
 	});
 });
