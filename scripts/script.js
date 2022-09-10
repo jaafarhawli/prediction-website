b564d89650c50e0cd29445de716d9cb5d9065aeb
Jaafar Hawli <jaafarhawle@gmail.com>
2022-09-10 15:02:01 +0300

    added the nationality api

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 let submit = document.getElementById('btn');
 let gender = document.getElementById('gender');
 let age = document.getElementById('age');
+let nationality = document.getElementById('nationality');
 
 btn.addEventListener('click', () => {
 	var name = document.getElementById('myText').value;
 	let gender_URL = `https://api.genderize.io/?name=${name}`;
 	let age_URL = `https://api.agify.io/?name=${name}`;
+	let nationality_URL = `https://api.nationalize.io/?name=${name}`;
 
 	//Fetch the gender
 	fetch(gender_URL).then((Response) => Response.json()).then((ResponseJson) => {
@@ -15,5 +17,13 @@ btn.addEventListener('click', () => {
 	//Fetch the age
 	fetch(age_URL).then((Response) => Response.json()).then((ResponseJson) => {
 		age.innerHTML = ResponseJson.age;
+	});
+
+	//Fetch the nationality
+	fetch(nationality_URL).then((Response) => Response.json()).then((ResponseJson) => {
+		nationality.innerHTML = ResponseJson.country[0].country_id;
+		if (ResponseJson.country[1]) {
+			nationality.innerHTML += ' ' + ResponseJson.country[1].country_id;
+		}
 	});
 });
